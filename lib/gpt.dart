1d42391948d0c1d48aad56eaa86dbbdb191aef14
furkan bora <frknbora@hotmail.com>
2023-11-30 02:32:34 +0300

    work like a charm

--- lib/gpt.dart
+++ lib/gpt.dart
@@ -22,3 +22,34 @@ Future makeTweet({required String send}) async {
           }));
   return jsonDecode(utf8.decode(response.bodyBytes));
 }
+
+Future _tagger({required String title}) async {
+  http.Response response =
+      await http.post(Uri.parse('https://api.openai.com/v1/chat/completions'),
+          headers: <String, String>{
+            'Content-Type': 'application/json',
+            'Authorization':
+                'Bearer ${secret.bearerToken}' //use your bearer token
+          },
+          body: jsonEncode({
+            "model": "ft:gpt-3.5-turbo-1106:personal::8QERatUe",
+            "messages": [
+              {"role": "system", "content": 'tagger'},
+              {"role": "user", "content": title}
+            ],
+            "temperature": 0
+          }));
+  return jsonDecode(utf8.decode(response.bodyBytes));
+}
+
+Future<List<String>> tagger({required String title}) async {
+  List<String> tags = [];
+  var gptObject = await _tagger(title: title.toString());
+  String gptTag = gptObject['choices'][0]['message']['content'];
+  if (gptTag.contains(' ')) {
+    tags = gptTag.split(' ');
+  } else {
+    tags.add(gptTag);
+  }
+  return tags;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:auto_tweet/components/send_tweet.dart';
 import 'package:auto_tweet/gdocs.dart';
+import 'package:auto_tweet/scraper.dart';
 import 'package:flutter/material.dart';
+import 'package:auto_tweet/gpt.dart' as gpt;
 
 void main() {
   GSheetsApi().init();
@@ -21,17 +23,18 @@ class HomeScreen extends StatelessWidget {
             style: const ButtonStyle(
                 backgroundColor: MaterialStatePropertyAll(Colors.indigo)),
             onPressed: () {
-              showDialog(
+              /* showDialog(
                 context: context,
                 builder: (context) {
-                  Future.delayed(Duration(seconds: 20), () {
+                  Future.delayed(const Duration(seconds: 20), () {
                     Navigator.pop(context);
                   });
                   return SendTweet(
                       url: Uri.parse(
                           '''https://twitter.com/compose/tweet?&text=Merhaba %23dünya bununla beraber tweet ortasında hashtag meselesini çözmüş oldum.'''));
                 },
-              );
+              ); */
+              saveNewNews();
             },
             child: const Text(
               'send tweet',
@@ -39,5 +42,35 @@ class HomeScreen extends StatelessWidget {
             )),
       ),
     );
+  }
+}
+
+void saveNewNews() async {
+  var sonDakika = await T24().sonDakika();
+  var lastTenNews = await GSheetsApi().getLestTenNews();
+  for (int i = 0; i < lastTenNews.length; i++) {
+    for (int s = 0; s < sonDakika.length; s++) {
+      if (lastTenNews[i][2] == sonDakika[s]['title']) {
+        sonDakika.removeRange(s, sonDakika.length);
+        break;
+      }
+    }
+  }
+  List<List<dynamic>> saveSonDakika = [];
+  for (var element in sonDakika) {
+    saveSonDakika.add([
+      '',
+      element["time"].toString(),
+      element["title"].toString(),
+      element["link"].toString()
+    ]);
+    saveSonDakika.last.insertAll(saveSonDakika.last.length,
+        await gpt.tagger(title: element['title'].toString()));
+  }
+  if (saveSonDakika.isNotEmpty) {
+    saveSonDakika.first.first = DateTime.now().toIso8601String();
+    GSheetsApi().write(
+        worksheet: GSheetsApi.t24SonDakikaWorkSheet,
+        data: saveSonDakika.reversed.toList());
   }
 }
